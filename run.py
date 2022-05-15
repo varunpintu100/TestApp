1c9951f0d6a179270f34527242715436355131ad
varunpintu100 <49563340+varunpintu100@users.noreply.github.com>
2022-05-15 12:05:14 +0530

    init changes in db

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 from app import app
 from database import db
 
-db.init__app(app)
+db.init_app(app)
 
 #this is used to run the script before the request so that we can create the tables all by ourself
 @app.before_first_request
